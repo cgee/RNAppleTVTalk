547f2e4ff973e6e097bec75498a831df3b738341
Chris Gee <cgee@cgee.com>
2018-07-13 15:30:18 -0700

    fix a few dependencies, signing, and config to run on my appleTV by speccing ip for dev server.

--- ios/RNAppleTVTalk/AppDelegate.m
+++ ios/RNAppleTVTalk/AppDelegate.m
@@ -17,25 +17,28 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
-
-    jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios"
-                                                                    fallbackResource:@"RNAppleTVTalk.ios"];
-    if(jsCodeLocation == nil) {
-      NSLog(@"The React Native packager is not running.  Please run 'npm start' from the RN directory.");
-      return nil;
-    }
+  BOOL runOnTv = YES;
+  
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios"
+                                                                  fallbackResource:@"RNAppleTVTalk.ios"];
+  if (runOnTv) {
+    // run on my apple tv
+    jsCodeLocation = [NSURL URLWithString:@"http://192.168.1.11:8081/index.ios.bundle?dev=true"];
+  }
+  if(jsCodeLocation == nil) {
+    NSLog(@"The React Native packager is not running.  Please run 'npm start' from the RN directory.");
+    return nil;
+  }
   
   //Debugging on real device on local network
   //jsCodeLocation = [NSURL URLWithString:@"http://10.0.0.6:8081/index.ios.bundle?dev=true"];
   
-
-
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"RNAppleTVTalk"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
